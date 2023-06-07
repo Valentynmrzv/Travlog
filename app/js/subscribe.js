8756ce46d61af4933643171bfe005953fe900b4d
Valentynmrzv <bmvalentin27@gmail.com>
2023-06-07 23:35:42 +0300

    sub button

--- app/js/subscribe.js
+++ app/js/subscribe.js
@@ -1,19 +1,27 @@
 
 document.querySelector(".form__button").addEventListener('click', function () {
-    document.querySelector(".form__button").classList.remove('deactivate');
+    // document.querySelector(".form__button").classList.remove('deactivate');
     document.querySelector(".form__button").classList.add('active');
     let ths = document.querySelector(".form__button");
     setTimeout(function () {
         ths.innerHTML = "✓"
     }, 700)
     setTimeout(function () {
+        document.querySelector(".form__button").classList.remove('active');
+        document.querySelector(".form__button").classList.add('active-btn');
+    }, 1700)
+    setTimeout(function () {
         // document.querySelector(".form__button").classList.remove('active');
         document.querySelector(".form__button").classList.add('deactivate');
-        ths.innerHTML = "Subscribe"
-    }, 4700)
+        setTimeout(function () {
+            ths.innerHTML = "Subscribe"
+        }, 700)
+    }, 5000)
     setTimeout(function () {
-        document.querySelector(".form__button").classList.remove('active');
         document.querySelector(".form__button").classList.remove('deactivate');
-    }, 9000)
+        document.querySelector(".form__button").classList.remove('active-btn');
+        
+    }, 6200)
+
 });
 
