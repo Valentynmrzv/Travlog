6addbeb7bc0e5c03f4f83bc4afd044bdb1a480f0
Valentynmrzv <bmvalentin27@gmail.com>
2023-06-08 20:32:47 +0300

    ss

--- app/js/dropdawn.js
+++ app/js/dropdawn.js
@@ -9,7 +9,7 @@ let isMobile = {
 		let body=document.querySelector('.dropdawn');
 if(isMobile.any()){
 		body.classList.add('touch');
-		let arrow=document.querySelectorAll('.arrow');
+		let arrow=document.querySelectorAll('.drop__button');
 	for(i=0; i<arrow.length; i++){
 			let thisLink=arrow[i].previousElementSibling;
 			let subMenu=arrow[i].nextElementSibling;
@@ -18,7 +18,7 @@ if(isMobile.any()){
 			thisLink.classList.add('parent');
 		arrow[i].addEventListener('click', function(){
 			subMenu.classList.toggle('open');
-			thisArrow.classList.toggle('active');
+			thisArrow.classList.toggle('open-button');
 		});
 	}
 }else{
